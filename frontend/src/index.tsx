e061575d054014e83cd4a7c432fc67ec9febe0fe
Evan Strat <evan10s@users.noreply.github.com>
2020-03-22 23:19:10 -0400

    GraphQL kind of works on the frontend

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,27 +4,13 @@ import './index.css';
 import 'semantic-ui-css/semantic.min.css'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {ApolloClient, ApolloLink, ApolloProvider, concat, HttpLink, InMemoryCache} from "@apollo/client";
-import Cookies from "js-cookie";
-
-// To work with Django's CSRF protection, this middleware takes the unique CSRF token provided in a cookie by Django
-// and sends it with every GraphQL request from Apollo as a header so that the requests pass Django's
-// CSRF protection.  As recommended by https://docs.djangoproject.com/en/2.2/ref/csrf/#ajax
-const csrfMiddleware = new ApolloLink(((operation, forward) => {
-    operation.setContext({
-        headers: {
-            "X-CSRFToken": Cookies.get("csrftoken")
-        }
-    });
-    return forward(operation);
-}));
+import {ApolloClient, ApolloProvider, HttpLink, InMemoryCache} from "@apollo/client";
 
 const client = new ApolloClient({
     cache: new InMemoryCache(),
-    link: concat(csrfMiddleware, new HttpLink({
+    link: new HttpLink({
         uri: "/api/graphql",
-        credentials: "include"
-    }))
+    })
 });
 
 ReactDOM.render(
