b4fa866676ca67f30b99b737d81903d0949d89b2
Ayush Goyal <agoyal2001@gmail.com>
2020-05-30 21:13:18 -0400

    Reformat RequisitionForm with grid and remove background color

--- frontend/src/components/requisition/RequisitionForm.tsx
+++ frontend/src/components/requisition/RequisitionForm.tsx
@@ -1,12 +1,15 @@
 import React, {ChangeEvent, FormEvent, SyntheticEvent} from 'react';
-import {Button, Divider, Form, DropdownProps, FormProps} from 'semantic-ui-react';
+import {Button, Divider, Form, DropdownProps, FormProps, Grid} from 'semantic-ui-react';
 import {DateInput} from 'semantic-ui-calendar-react';
 import RequisitionItemSegment from "./RequisitionItemSegment";
 import {InputOnChangeData} from "semantic-ui-react/dist/commonjs/elements/Input/Input";
 import {TextAreaProps} from "semantic-ui-react/dist/commonjs/addons/TextArea/TextArea";
 import {RequisitionItem} from "../../util/types/RequisitionItem";
 
-type RequisitionFormEvent = ChangeEvent<HTMLInputElement> | FormEvent<HTMLTextAreaElement> | SyntheticEvent<HTMLElement>;
+type RequisitionFormEvent =
+    ChangeEvent<HTMLInputElement>
+    | FormEvent<HTMLTextAreaElement>
+    | SyntheticEvent<HTMLElement>;
 type RequisitionFormData = InputOnChangeData | TextAreaProps | DropdownProps | any;
 
 interface RequisitionFormProps {
@@ -22,7 +25,7 @@ interface RequisitionFormState {
     project: string,
     paymentRequiredBy: string,
     vendor: string,
-    items: [RequisitionItem]
+    items: RequisitionItem[]
 }
 
 class RequisitionForm extends React.Component<RequisitionFormProps, RequisitionFormState> {
@@ -35,14 +38,19 @@ class RequisitionForm extends React.Component<RequisitionFormProps, RequisitionF
             project: '',
             paymentRequiredBy: '',
             vendor: '',
-            // @ts-ignore
-            items: [{}]
+            items: [{
+                name: '',
+                price: 0,
+                quantity: 0,
+                link: '',
+                notes: ''
+            }]
         }
     }
 
     handleChange = (event: RequisitionFormEvent, data: RequisitionFormData) => {
         // @ts-ignore
-        this.setState({ [data.name]: data.value });
+        this.setState({[data.name]: data.value});
     }
 
     handleItemChange = (event: RequisitionFormEvent, data: RequisitionFormData, index: number) => {
@@ -60,8 +68,18 @@ class RequisitionForm extends React.Component<RequisitionFormProps, RequisitionF
     addItem = (event: any) => {
         event.preventDefault();
 
-        // @ts-ignore
-        this.setState({ items: [...this.state.items, {}] })
+        this.setState({
+            items: [
+                ...this.state.items,
+                {
+                    name: '',
+                    price: 0,
+                    quantity: 0,
+                    link: '',
+                    notes: ''
+                }
+            ]
+        })
     }
 
     deleteItem = (index: number) => {
@@ -76,61 +94,87 @@ class RequisitionForm extends React.Component<RequisitionFormProps, RequisitionF
     }
 
     handleSubmit = (event: FormEvent<HTMLFormElement>, data: FormProps) => {
+        console.log(event);
+        console.log(data);
         console.log(this.state);
     }
 
     render() {
         return (
             <Form onSubmit={this.handleSubmit} autoComplete='off'>
-                <Form.Input
-                    name='name'
-                    value={this.state.name}
-                    onChange={this.handleChange}
-                    label='Name'
-                    placeholder='Name...'
-                />
-                <Form.TextArea
-                    name='description'
-                    value={this.state.description}
-                    onChange={this.handleChange}
-                    label='Description'
-                    placeholder='Description...'
-                />
-                <Form.Dropdown
-                    name='project'
-                    label='Project'
-                    value={this.state.project}
-                    onChange={this.handleChange}
-                    options={this.props.projectOptions}
-                    placeholder='Select...'
-                    selection
-                />
-                <Form.Dropdown
-                    name='vendor'
-                    label='Vendor'
-                    value={this.state.vendor}
-                    onChange={this.handleChange}
-                    options={[{text: 'Amazon', value: 'Amazon'}]}
-                    placeholder='Select...'
-                    selection
-                />
-                <Form.Field>
-                    <label>Payment Required By</label>
-                    <DateInput
-                        name='paymentRequiredBy'
-                        value={this.state.paymentRequiredBy}
-                        onChange={this.handleChange}
-                        iconPosition='left'
-                        popupPosition='bottom left'
-                        dateFormat='M-DD-YYYY'
-                        // @ts-ignore - gets rid of flicker https://github.com/arfedulov/semantic-ui-calendar-react/issues/152
-                        animation={'none'}
-                    />
-                </Form.Field>
+                <Grid>
+                    <Grid.Row>
+                        <Grid.Column width={8}>
+                            <Form.Input
+                                name='name'
+                                value={this.state.name}
+                                onChange={this.handleChange}
+                                label='Name'
+                                placeholder='Name...'
+                                required
+                            />
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+                            <Form.Dropdown
+                                name='project'
+                                label='Project'
+                                value={this.state.project}
+                                onChange={this.handleChange}
+                                options={this.props.projectOptions}
+                                placeholder='Select...'
+                                selection
+                                required
+                            />
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={16}>
+                            <Form.TextArea
+                                name='description'
+                                value={this.state.description}
+                                onChange={this.handleChange}
+                                label='Description'
+                                placeholder='Description...'
+                                required
+                            />
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={8}>
+                            <Form.Dropdown
+                                name='vendor'
+                                label='Vendor'
+                                value={this.state.vendor}
+                                onChange={this.handleChange}
+                                options={[{text: 'Amazon', value: 'Amazon'}]}
+                                placeholder='Select...'
+                                selection
+                                required
+                            />
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+                            <Form.Field required>
+                                <label>Payment Required By</label>
+                                <DateInput
+                                    name='paymentRequiredBy'
+                                    value={this.state.paymentRequiredBy}
+                                    onChange={this.handleChange}
+                                    iconPosition='left'
+                                    popupPosition='bottom left'
+                                    dateFormat='M-DD-YYYY'
+                                    // @ts-ignore - gets rid of flicker https://github.com/arfedulov/semantic-ui-calendar-react/issues/152
+                                    animation={'none'}
+                                    required
+                                />
+                            </Form.Field>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
                 <Divider/>
                 {
                     this.state.items.map((item: RequisitionItem, index: number) =>
                         <RequisitionItemSegment
+                            key={index}
                             id={index}
                             data={item}
                             deleteItem={this.deleteItem}
--- frontend/src/components/requisition/RequisitionItemSegment.tsx
+++ frontend/src/components/requisition/RequisitionItemSegment.tsx
@@ -1,5 +1,5 @@
 import React, {Fragment} from "react";
-import {Button, Form, Header, Segment} from "semantic-ui-react";
+import {Button, Form, Grid, Header, Segment} from "semantic-ui-react";
 import {RequisitionItem} from "../../util/types/RequisitionItem";
 
 interface RequisitionItemSegmentProps {
@@ -17,44 +17,78 @@ const RequisitionItemSegment: React.FunctionComponent<RequisitionItemSegmentProp
                 Item {props.id + 1}
             </Header>
             <Segment attached='bottom'>
-                <Form.Input
-                    name='name'
-                    value={props.data.name || ''}
-                    label='Name'
-                    placeholder='Name...'
-                    onChange={props.onChange}
-                />
-                <Form.Input
-                    name='price'
-                    value={props.data.price || ''}
-                    label='Price'
-                    placeholder='Price...'
-                    onChange={props.onChange}
-                />
-                <Form.Input
-                    name='quantity'
-                    value={props.data.quantity || ''}
-                    label='Quantity'
-                    placeholder='Quantity...'
-                    onChange={props.onChange}
-                />
-                <Form.Input
-                    name='link'
-                    value={props.data.link || ''}
-                    label='Link'
-                    placeholder='Link...'
-                    onChange={props.onChange}
-                />
-                <Form.TextArea
-                    name='notes'
-                    value={props.data.notes || ''}
-                    label='Notes'
-                    placeholder='Notes...'
-                    onChange={props.onChange}
-                />
-                <Button disabled={props.deleteDisabled} basic color='red' onClick={() => props.deleteItem(props.id)}>
-                    Delete
-                </Button>
+                <Grid>
+                    <Grid.Row>
+                        <Grid.Column width={8}>
+                            <Form.Input
+                                name='name'
+                                value={props.data.name}
+                                label='Name'
+                                placeholder='Name...'
+                                onChange={props.onChange}
+                                required
+                            />
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+                            <Form.Input
+                                name='link'
+                                value={props.data.link}
+                                label='Link'
+                                placeholder='Link...'
+                                onChange={props.onChange}
+                                type="url"
+                                required
+                            />
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={8}>
+                            <Form.Input
+                                name='price'
+                                value={props.data.price || ''}
+                                label='Price'
+                                placeholder='Price...'
+                                onChange={props.onChange}
+                                type="number"
+                                required
+                            />
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+                            <Form.Input
+                                name='quantity'
+                                value={props.data.quantity || ''}
+                                label='Quantity'
+                                placeholder='Quantity...'
+                                onChange={props.onChange}
+                                type="number"
+                                required
+                            />
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={16}>
+                            <Form.TextArea
+                                name='notes'
+                                value={props.data.notes}
+                                label='Notes'
+                                placeholder='Notes...'
+                                onChange={props.onChange}
+                            />
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={3}>
+                            <Button
+                                disabled={props.deleteDisabled}
+                                color='red'
+                                onClick={() => props.deleteItem(props.id)}
+                                basic
+                            >
+                                Delete
+                            </Button>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
             </Segment>
         </Fragment>
     )
