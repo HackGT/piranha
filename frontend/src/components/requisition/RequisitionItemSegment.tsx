faa1a3662ca203c8ce77bc85fa0985206e06dade
Ayush Goyal <agoyal2001@gmail.com>
2020-06-02 13:03:27 -0400

    Add help popup info for requisition fields

--- frontend/src/components/requisition/RequisitionItemSegment.tsx
+++ frontend/src/components/requisition/RequisitionItemSegment.tsx
@@ -1,5 +1,5 @@
 import React, {Fragment} from "react";
-import {Button, Form, Grid, Header, Segment} from "semantic-ui-react";
+import {Button, Form, Grid, Header, Segment, Popup, Icon} from "semantic-ui-react";
 import {RequisitionItem} from "../../util/types/RequisitionItem";
 
 interface RequisitionItemSegmentProps {
@@ -20,60 +20,95 @@ const RequisitionItemSegment: React.FunctionComponent<RequisitionItemSegmentProp
                 <Grid>
                     <Grid.Row>
                         <Grid.Column width={8}>
-                            <Form.Input
-                                name='name'
-                                value={props.data.name}
-                                label='Name'
-                                placeholder='Name...'
-                                onChange={props.onChange}
-                                required
-                            />
+                            <Form.Field required>
+                                <label>Name</label>
+                                <Popup
+                                    content='The name of the sub item.'
+                                    trigger={<Icon style={{cursor: 'help'}} name='question circle outline' />}
+                                    basic
+                                />
+                                <Form.Input
+                                    name='name'
+                                    value={props.data.name}
+                                    placeholder='Name...'
+                                    onChange={props.onChange}
+                                    required
+                                />
+                            </Form.Field>
                         </Grid.Column>
                         <Grid.Column width={8}>
-                            <Form.Input
-                                name='link'
-                                value={props.data.link}
-                                label='Link'
-                                placeholder='Link...'
-                                onChange={props.onChange}
-                                type="url"
-                                required
-                            />
+                            <Form.Field required>
+                                <label>Link</label>
+                                <Popup
+                                    content='The link to the item you want to purchase.'
+                                    trigger={<Icon style={{cursor: 'help'}} name='question circle outline' />}
+                                    basic
+                                />
+                                <Form.Input
+                                    name='link'
+                                    value={props.data.link}
+                                    placeholder='Link...'
+                                    onChange={props.onChange}
+                                    type="url"
+                                    required
+                                />
+                            </Form.Field>
                         </Grid.Column>
                     </Grid.Row>
                     <Grid.Row>
                         <Grid.Column width={8}>
-                            <Form.Input
-                                name='price'
-                                value={props.data.price || ''}
-                                label='Price'
-                                placeholder='Price...'
-                                onChange={props.onChange}
-                                type="number"
-                                required
-                            />
+                            <Form.Field required>
+                                <label>Price</label>
+                                <Popup
+                                    content='The price of the item.'
+                                    trigger={<Icon style={{cursor: 'help'}} name='question circle outline' />}
+                                    basic
+                                />
+                                <Form.Input
+                                    name='price'
+                                    value={props.data.price || ''}
+                                    placeholder='Price...'
+                                    onChange={props.onChange}
+                                    type="number"
+                                    required
+                                />
+                            </Form.Field>
                         </Grid.Column>
                         <Grid.Column width={8}>
-                            <Form.Input
-                                name='quantity'
-                                value={props.data.quantity || ''}
-                                label='Quantity'
-                                placeholder='Quantity...'
-                                onChange={props.onChange}
-                                type="number"
-                                required
-                            />
+                            <Form.Field required>
+                                <label>Quantity</label>
+                                <Popup
+                                    content='The quantity of this item you need.'
+                                    trigger={<Icon style={{cursor: 'help'}} name='question circle outline' />}
+                                    basic
+                                />
+                                <Form.Input
+                                    name='quantity'
+                                    value={props.data.quantity || ''}
+                                    placeholder='Quantity...'
+                                    onChange={props.onChange}
+                                    type="number"
+                                    required
+                                />
+                            </Form.Field>
                         </Grid.Column>
                     </Grid.Row>
                     <Grid.Row>
                         <Grid.Column width={16}>
-                            <Form.TextArea
-                                name='notes'
-                                value={props.data.notes}
-                                label='Notes'
-                                placeholder='Notes...'
-                                onChange={props.onChange}
-                            />
+                            <Form.Field>
+                                <label>Notes </label>
+                                <Popup
+                                    content='Any notes about the item such as specific colors or sizes.'
+                                    trigger={<Icon style={{cursor: 'help'}} name='question circle outline' />}
+                                    basic
+                                />
+                                <Form.TextArea
+                                    name='notes'
+                                    value={props.data.notes}
+                                    placeholder='Notes...'
+                                    onChange={props.onChange}
+                                />
+                            </Form.Field>
                         </Grid.Column>
                     </Grid.Row>
                     <Grid.Row>
