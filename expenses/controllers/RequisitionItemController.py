0c5c0bd1fe7d4f13f0de1e39d23fe55a3aa0dcd7
Ayush Goyal <agoyal2001@gmail.com>
2020-07-01 23:24:32 -0400

    Change permission refactor

--- expenses/controllers/RequisitionItemController.py
+++ expenses/controllers/RequisitionItemController.py
@@ -3,10 +3,6 @@ from expenses.models import RequisitionItem
 
 class RequisitionItemController:
     @classmethod
-    def can_view(cls, user):
-        return user.has_perm("expenses.view_requisition_item")
-
-    @classmethod
     def get_requisition_item(cls, info, id):
-        if cls.can_view(info.context.user):
+        if info.context.user.has_perm("expenses.view_requisition_item"):
             return RequisitionItem.objects.get(id=id)
--- expenses/controllers/VendorController.py
+++ expenses/controllers/VendorController.py
@@ -3,15 +3,11 @@ from expenses.models import Vendor
 
 class VendorController:
     @classmethod
-    def can_view(cls, user):
-        return user.has_perm("expenses.view_vendor")
-
-    @classmethod
     def get_vendor(cls, info, id):
-        if cls.can_view(info.context.user):
+        if info.context.user.has_perm("expenses.view_vendor"):
             return Vendor.objects.get(id=id)
 
     @classmethod
     def get_vendors(cls, info, where):
-        if cls.can_view(info.context.user):
+        if info.context.user.has_perm("expenses.view_vendor"):
             return Vendor.objects.filter(**where)
