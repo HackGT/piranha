b200983f373c69d539006dbde0b37409618b571b
Ayush Goyal <agoyal2001@gmail.com>
2020-07-01 23:24:32 -0400

    Change permission refactor

--- expenses/controllers/UserController.py
+++ expenses/controllers/UserController.py
@@ -3,15 +3,11 @@ from django.contrib import auth
 
 class UserController:
     @classmethod
-    def can_view(cls, user):
-        return user.has_perm("seaport.view_user")
-
-    @classmethod
     def get_user(cls, info):
-        if cls.can_view(info.context.user):
+        if info.context.user.has_perm("seaport.view_user"):
             return info.context.user
 
     @classmethod
     def get_users(cls, info, where):
-        if cls.can_view(info.context.user):
+        if info.context.user.has_perm("seaport.view_user"):
             return auth.get_user_model().objects.filter(**where)
