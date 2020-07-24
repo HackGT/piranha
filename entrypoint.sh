3dd4ca4c09106cf780a5276ac71590add55ff00d
Ayush Goyal <agoyal2001@gmail.com>
2020-07-23 23:29:52 -0400

    Add Docker entrypoint and static url

---
+++ entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python manage.py migrate --noinput        # Apply database migrations
+python manage.py collectstatic --noinput  # Collect static files
+
+exec gunicorn piranha.wsgi:application --bind :8000 --workers 3
--- piranha/settings.py
+++ piranha/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'frontend', "build", "static"),
 )
