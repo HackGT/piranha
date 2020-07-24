220b3604314e07dc2c785150ae7faedaf6d026ea
Ayush Goyal <agoyal2001@gmail.com>
2020-07-23 20:17:41 -0400

    Prepare for deployment

--- piranha/settings.py
+++ piranha/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ["DEBUG"].lower() == "true")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.hack.gt', 'localhost']
 
 # Application definition
 
