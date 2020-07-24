fa07582cf2315d13a2ff086d83095e3624f11ae4
Ayush Goyal <agoyal2001@gmail.com>
2020-07-24 18:33:42 -0400

    Hack default django 404 handler to fix login issue

--- piranha/views.py
+++ piranha/views.py
@@ -3,5 +3,10 @@ from django.shortcuts import render
 
 
 @login_required
+def handler404(request, exception):
+    return render(request, "build/index.html")
+
+
+@login_required
 def index(request):
     return render(request, "build/index.html")
