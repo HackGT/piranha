2436bd6b5eb0ff0bdaa390e0eecda212341be68f
Ayush Goyal <agoyal2001@gmail.com>
2020-07-25 19:37:41 -0400

    Update admin panel for new payment method fields

--- frontend/src/components/admin/manageContent/formModals/PaymentMethodFormModal.tsx
+++ frontend/src/components/admin/manageContent/formModals/PaymentMethodFormModal.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-import { Form, Input, Switch } from "antd";
+import { Form, Input, Switch, Tooltip } from "antd";
 import { ApolloCache } from "@apollo/client";
+import { QuestionCircleOutlined } from "@ant-design/icons/lib";
 import { FORM_RULES } from "../../../../util/util";
 import ManageContentModal from "../ManageContentModal";
 import { CREATE_PAYMENT_METHOD_MUTATION, UPDATE_PAYMENT_METHOD_MUTATION, PAYMENT_METHOD_LIST_QUERY } from "../../../../types/PaymentMethod";
 import { FormModalProps } from "./FormModalProps";
+
+const { TextArea } = Input;
 
 const PaymentMethodFormModal: React.FC<FormModalProps> = props => (
   <ManageContentModal
@@ -35,6 +38,38 @@ const PaymentMethodFormModal: React.FC<FormModalProps> = props => (
           initialValue={initialValues && initialValues.name}
         >
           <Input placeholder="Gringotts Wizarding Bank" />
+        </Form.Item>
+        <Form.Item
+          name="reimbursementInstructions"
+          label={(
+            <span>
+              {"Reimbursement Instructions "}
+              <Tooltip title="These notes will be shown to the submitter when they are requesting a reimbursement from this payment method. These instructions could include forms to fill out, or a Wiki link.">
+                <QuestionCircleOutlined />
+              </Tooltip>
+            </span>
+          )}
+          initialValue={initialValues && initialValues.reimbursementInstructions}
+        >
+          <TextArea
+            autoSize={{ minRows: 4 }}
+            placeholder="Visit www.docusign.com/form and fill out the reimbursement form. After completing, send it to the Director of Finance."
+          />
+        </Form.Item>
+        <Form.Item
+          name="isDirectPayment"
+          label={(
+            <span>
+              {"Direct Payment "}
+              <Tooltip title="For reimbursements, select yes for direct payment if the funding source can immediately send funds after approval. If the funding source requires forms or other documents to be submitted, select no for direct payment.">
+                <QuestionCircleOutlined />
+              </Tooltip>
+            </span>
+          )}
+          valuePropName="checked"
+          initialValue={initialValues && initialValues.isDirectPayment}
+        >
+          <Switch checkedChildren="Yes" unCheckedChildren="No" />
         </Form.Item>
         {initialValues && (
           <Form.Item
