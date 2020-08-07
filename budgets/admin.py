5f5756bc0cda361ba1c1dc9f8db7e9fe3746772f
Ayush Goyal <agoyal2001@gmail.com>
2020-08-06 23:27:54 -0400

    Change budget model to make operating expenses non requisition

--- budgets/admin.py
+++ budgets/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import BudgetGroup, Budget, OperatingBudget, Category, LineItem
+from .models import BudgetGroup, Budget, Category, LineItem, OperatingBudget, OperatingLineItem
 
 
 admin.site.register(BudgetGroup)
 admin.site.register(Budget)
-admin.site.register(OperatingBudget)
 admin.site.register(Category)
 admin.site.register(LineItem)
+admin.site.register(OperatingBudget)
+admin.site.register(OperatingLineItem)
