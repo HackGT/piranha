3070cbe5011a96880b0ed742bdb8e9a3775b947a
Ayush Goyal <agoyal2001@gmail.com>
2020-08-30 17:53:43 -0400

    Change slack message on updates

--- expenses/config.py
+++ expenses/config.py
@@ -14,11 +14,11 @@ def send_slack_notification(requisition):
     # Checks if slack token is provide as env var
     if slack_client.token and requisition.created_by.slack_id:
         if requisition.status == RequisitionStatus.SUBMITTED:
-            message = f"Thank you for submitting requisition {requisition.headline}! You will receive alerts from me when the status is changed."
+            message = f"Thank you for submitting requisition {requisition.reference_string} ({requisition.headline})! You will receive alerts from me when the status is changed."
         elif requisition.status == RequisitionStatus.PENDING_CHANGES:
-            message = f"Requisition {requisition.headline} ({requisition}) has requested changes. Notes by reviewer: {requisition.approval_set.latest('id').notes}"
+            message = f"Requisition {requisition.reference_string} ({requisition.headline}) has requested changes. Notes by reviewer: {requisition.approval_set.latest('id').notes}"
         else:
-            message = f"Requisition {requisition.headline} ({requisition}) status updated to *{requisition.status}*"
+            message = f"Requisition {requisition.reference_string} ({requisition.headline}) status updated to *{requisition.status}*"
 
         try:
             response = slack_client.chat_postMessage(channel=requisition.created_by.slack_id, text=message)
