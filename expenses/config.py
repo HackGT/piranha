a804f6c2f9edebff22c265994bd3864118b3f240
Ayush Goyal <agoyal2001@gmail.com>
2020-08-30 02:11:11 -0400

    Add sending slack messages on status change

--- expenses/config.py
+++ expenses/config.py
@@ -1,5 +1,8 @@
 from google.cloud import storage
 import os
+from slack import WebClient
 
 storage_client = storage.Client()
 bucket = storage_client.bucket(os.environ["GOOGLE_STORAGE_BUCKET"])
+
+slack_client = WebClient(token=os.environ.get('SLACK_API_TOKEN', ''))
