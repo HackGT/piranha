082ea23a07fac3bb5e7d30fe4b793e26c8aa4a9b
Ayush Goyal <agoyal2001@gmail.com>
2020-08-31 23:44:28 -0400

    Try scaling down gunicorn workers to reduce memory usage

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,4 @@
 python manage.py migrate --noinput        # Apply database migrations
 python manage.py collectstatic --noinput  # Collect static files
 
-exec gunicorn piranha.wsgi:application --bind :8000 --workers 3
+exec gunicorn piranha.wsgi:application --bind :8000 --workers 2
