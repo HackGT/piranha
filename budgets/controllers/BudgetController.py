ad277d74936de4645618bdb4db6e0b38a9c7c8ee
Ayush Goyal <agoyal2001@gmail.com>
2020-09-13 23:21:18 -0400

    Add query to get budget by id

--- budgets/controllers/BudgetController.py
+++ budgets/controllers/BudgetController.py
@@ -3,6 +3,11 @@ from budgets.models import Budget
 
 class BudgetController:
     @classmethod
+    def get_budget(cls, info, id):
+        if info.context.user.has_perm("budgets.view_budget"):
+            return Budget.objects.filter(id=id).first()
+
+    @classmethod
     def get_budgets(cls, info):
         if info.context.user.has_perm("budgets.view_budget"):
             return Budget.objects.all()
--- budgets/graphql/Query.py
+++ budgets/graphql/Query.py
@@ -6,7 +6,13 @@ from budgets.controllers.BudgetController import BudgetController
 
 
 class Query(ObjectType):
+    budget = graphene.Field(BudgetType, id=graphene.ID())
     budgets = graphene.List(BudgetType)
+
+    def resolve_budget(self, info, **kwargs):
+        id = kwargs.get("id")
+
+        return BudgetController.get_budget(info, id)
 
     def resolve_budgets(self, info, **kwargs):
         return BudgetController.get_budgets(info)
