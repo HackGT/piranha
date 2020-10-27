55e14d8799449ca235bd19bdd303f801c519b21e
Ayush Goyal <agoyal2001@gmail.com>
2020-10-26 21:47:39 -0400

    Add created by column and move status on project page

--- frontend/src/types/Project.ts
+++ frontend/src/types/Project.ts
@@ -42,6 +42,10 @@ export const PROJECT_DETAIL_QUERY = gql`
         canEdit
         otherFees
         isReimbursement
+        createdBy {
+          id
+          fullName
+        }
         project {
           referenceString
         }
