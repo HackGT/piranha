cc34af7736890c9641009ea6eef811a7e55c341f
Aayush <aayushseth8@gmail.com>
2021-03-30 23:32:10 -0700

    Map cards to budgets page and pull from graphql query (broken)

--- client/src/components/budgets/Budgets.tsx
+++ client/src/components/budgets/Budgets.tsx
@@ -1,15 +1,37 @@
 import React from "react";
-import { Typography } from "antd";
+import { Typography, List } from "antd";
+import { useQuery } from "@apollo/client";
 
-const { Title, Text } = Typography;
+import BudgetCard from "./BudgetCard";
+import { BUDGET_QUERY } from "../../queries/Budget"
 
-const Budgets: React.FC = () => (
-  <>
-    <Title style={{ textAlign: "left" }} level={2}>
-      Your Budgets
-    </Title>
-    <Text>Development currently in progress...</Text>
-  </>
-);
+const { Title } = Typography;
+
+const Budgets: React.FC = () => {
+
+  const { data } = useQuery(BUDGET_QUERY);
+  console.log(data);
+
+  
+  const budgetCardCategories = data.budgets.categories;
+
+  return (
+    <>
+      <Title style={{ textAlign: "left" }} level={2}>
+        Your Budgets
+      </Title>
+
+      <List
+        grid={{ gutter: 16, xs: 1, sm: 2, md: 2, lg: 3, xl: 4, xxl: 5 }} 
+        dataSource={budgetCardCategories}
+        renderItem={category => (
+          <List.Item>
+            <BudgetCard key={category.id} category={category} />
+          </List.Item>
+        )}
+      />
+    </>
+  );
+};
 
 export default Budgets;
