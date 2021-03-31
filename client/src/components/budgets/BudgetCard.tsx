65393e926b740a79a80a82b8dee8f13fbcea6707
Aayush <aayushseth8@gmail.com>
2021-03-30 22:39:27 -0700

    Create card to display budget

---
+++ client/src/components/budgets/BudgetCard.tsx
@@ -0,0 +1,49 @@
+import { Card, Table } from "antd";
+import React from "react";
+
+import { Category } from "../../generated/types";
+
+interface Props {
+  category: Category;
+}
+
+const BudgetCard: React.FC<Props> = (props) => {
+
+  const lineItemsData = props.category.lineItems.map(item => (
+      {
+        key: item.id,
+        name: item.name,
+        quantity: item.quantity,
+        unitCost: item.unitCost
+      }
+    )
+  ); 
+
+  const lineItemColumns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name"
+    },
+    {
+      title: "Quantity",
+      dataIndex: "quantity",
+      key: "quantity"
+    },
+    {
+      title: "Unit Cost",
+      dataIndex: "unitCost",
+      key: "unitCost"
+    },
+  ];
+
+  return (
+  <>
+    <Card title={props.category.name} style={{ width: 300 }}>
+      <Table dataSource={lineItemsData} columns={lineItemColumns} />
+    </Card>
+  </>
+  );
+};
+
+export default BudgetCard;
