249f7f9a434bd09e6de4597f46b2313b947e6427
ceelo777 <lochristopher9@gmail.com>
2021-04-01 23:14:30 -0400

    add budgets page along with cards to go with it

---
+++ client/src/components/budgets/BudgetsCard.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Card, List, Skeleton, Typography } from "antd";
+import { UserOutlined } from "@ant-design/icons/lib";
+import { Link } from "react-router-dom";
+
+import { Budget, User } from "../../generated/types";
+
+const { Title, Text } = Typography;
+
+interface Props {
+  item: Budget;
+  loading: boolean;
+}
+
+const BudgetsCard: React.FC<Props> = props => (  
+    <Card
+      title={
+        <Skeleton loading={props.loading} paragraph={false} active>
+          <div className="card-head-wrapper">
+            <Title level={4} className="card-head-title">
+              {props.item.name}
+            </Title>
+          </div>
+        </Skeleton>
+      }
+      loading={props.loading}
+      hoverable
+    />       
+);
+
+export default BudgetsCard;
