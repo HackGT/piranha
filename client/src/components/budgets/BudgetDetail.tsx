0cd352a6bfa572a19780ca3da72c5a405527be30
Aayush <aayushseth8@gmail.com>
2021-04-07 16:33:28 -0700

    Update BudgetDetail component so it can be used on its own page

--- client/src/components/budgets/BudgetDetail.tsx
+++ client/src/components/budgets/BudgetDetail.tsx
@@ -1,67 +1,30 @@
 import React from "react";
-import { List, Collapse } from "antd";
-import { useQuery } from "@apollo/client";
+import { List } from "antd";
+import Title from "antd/lib/typography/Title";
 
-import ErrorDisplay from "../../util/ErrorDisplay"
+
 import BudgetCard from "./BudgetCard";
-import { BUDGET_QUERY } from "../../queries/Budget"
 import { Budget, Category } from "../../generated/types";
 
-const { Panel } = Collapse;
+interface Props {
+  budget: Budget;
+}
 
-const BudgetDetail: React.FC = () => {
-
-  const { data, loading, error } = useQuery(BUDGET_QUERY);
-
-  if (error) {
-    return <ErrorDisplay error={error} />;
-  }
-
-  const emptyBudget = [
-    {
-      id: 1,
-      name: "loading",
-      categories: [
-        {
-          id: 1,
-          name: "-",
-          lineItems: [
-            {
-              id: 1,
-              name: "-",
-              quantity: 1,
-              unitCost: 1
-            }
-          ], 
-        }
-      ]
-    }
-  ];
-
-  const allBudgets = loading ? emptyBudget : data.budgets;
-
-  return (
-    <>
-      <List
-        dataSource={allBudgets}
-        renderItem={(budget: Budget) => (
-          <Collapse key={budget.id}>
-            <Panel key={budget.id} header={budget.name}>
-                <List
-                  grid={{ gutter: 16, xs: 1, sm: 2, md: 2, lg: 3, xl: 4, xxl: 5 }} 
-                  dataSource={budget.categories}
-                  renderItem={(category: Category) => (
-                  <List.Item>
-                    <BudgetCard key={category.id} category={category} />
-                  </List.Item>
-                  )}
-                />
-            </Panel>
-          </Collapse>
-        )}
-      />
-    </>
-  );
-};
+const BudgetDetail: React.FC<Props> = (props) => (
+  <>
+    <Title style={{ textAlign: "left" }} level={2}>
+      {props.budget.name} Details
+    </Title>
+    <List
+      grid={{ gutter: 16, xs: 1, sm: 2, md: 2, lg: 3, xl: 4, xxl: 5 }} 
+      dataSource={props.budget.categories}
+      renderItem={(category: Category) => (
+      <List.Item>
+        <BudgetCard key={category.id} category={category} />
+      </List.Item>
+      )}
+    />
+  </>
+);
 
 export default BudgetDetail;
